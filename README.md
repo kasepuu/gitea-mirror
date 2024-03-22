8b624b6448616fc721b8d4860363a602b06ebab3
kasepuu <joel.meeks@outlook.com>
2024-03-22 08:03:10 +0200

    git-merger 1.0

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # gitea-mirror
-Shell script for mirroring Gitea platform
+
+Shell script for mirroring Gitea repositories to GitHub while preserving commit history.
+
+### Usage
+
+1. **Set up your settings:**
+```
+GITHUB_USERNAME="your_github_username"
+GITHUB_ACCESS_TOKEN="your_personal_access_token"
+GITEA_USERNAME="your_gitea_username"
+GITEA_ACCESS_TOKEN="your-gitea-token"
+GITEA_SERVICE_PROVIDER="your_provider" #https://mygiteaserver.me 
+```
+Note: Ensure that GITEA_SERVICE_PROVIDER does not end with a "/".
+
+[GITHUB_ACCESS_TOKEN](https://github.com/settings/tokens)
+
+
+2. Execute the script:
+```
+    :~$ bash git-merger.sh
+```
+#### Additional Notes
+* The script clones Gitea repositories and mirrors them to GitHub
+* Preserve your project's commit history by using this script
+
+## Disclaimer
+
+This script is provided as-is, without any warranty or guarantee of its effectiveness or suitability for any specific purpose. The author and contributors of this script shall not be liable for any damages or issues arising from the use of this script.
+
+Please ensure that you review and understand the script's functionality and settings before running it, as it may have unintended consequences if not used correctly. Always backup your data and repositories before executing the script, especially when dealing with sensitive or critical information.
+
+Use this script at your own risk. The author and contributors do not provide any support or assistance beyond what is documented in this readme. If you encounter any issues or have questions about the script, you are encouraged to seek assistance from relevant forums, communities, or technical experts.
+
+By using this script, you agree to the terms outlined in this disclaimer.
+###
+
+#### Author [kasepuu](https://github.com/kasepuu)
---
+++ git-merger.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# SETTINGS
+GITHUB_USERNAME="your_github_username"
+GITHUB_ACCESS_TOKEN="your_personal_access_token"
+GITEA_USERNAME="your_gitea_username"
+GITEA_ACCESS_TOKEN="your-gitea-token"
+GITEA_SERVICE_PROVIDER="your_provider" #https://mygiteaserver.me
+
+# GRABBING LIST OF REPOSITORIES
+PAGE=1 # STARTING PAGE
+REPO_LIST=""
+# REPO_LIST=$(curl -s -H "Authorization: token $GITEA_ACCESS_TOKEN" "$GITEA_SERVICE_PROVIDER/api/v1/users/$GITEA_USERNAME/repos" | jq -r '.[].ssh_url')
+fetch_repositories() {
+    local PAGE=1
+    while true; do
+        RESPONSE=$(curl -s -H "Authorization: token $GITEA_TOKEN" "$GITEA_SERVICE_PROVIDER/api/v1/users/$GITEA_USERNAME/repos?page=$PAGE")
+        if [ "$RESPONSE" == "[]" ]; then
+            break
+        fi
+        REPO_LIST="$REPO_LIST $(echo "$RESPONSE" | jq -r '.[].ssh_url')"
+        ((PAGE++))
+    done
+}
+echo "List of repositories:"
+echo "$REPO_LIST"
+echo "Press enter to continue..."
+read ans
+
+# Looping through each repository and mirroring it to GitHub
+for REPO_URL in $REPO_LIST; do
+    echo "[START] Cloning... $REPO_URL"
+
+    # Extract repository name using REGEX
+    REPO_NAME=$(echo "$REPO_URL" | sed 's#.*/\([^/]\+\)\.git$#\1#')
+
+    REBUILT_URL="$GITEA_SERVICE_PROVIDER/$GITEA_USERNAME/$REPO_NAME" # Rebuilding the URL
+
+    git clone --mirror "$REBUILT_URL" || {
+        echo "[ERROR] Cloning failed for $REPO_NAME"
+        exit 1
+    }                   # Clone the repository from Gitea as a mirror
+    cd "$REPO_NAME.git" # Navigating in to the repository directory
+
+    echo "[INFO] Adding GitHub as a remote"
+    git remote add github "https://$GITHUB_ACCESS_TOKEN@github.com/$GITHUB_USERNAME/$REPO_NAME"
+
+    # Push to GitHub
+    echo "[INFO] Pushing into GitHub"
+    git push --mirror github || (
+        echo "[WARNING] Repository doesn't exist on GitHub. Creating a new repository..."
+        curl -f -H "Authorization: token $GITHUB_ACCESS_TOKEN" -d '{"name":"'"$REPO_NAME"'", "private": true}' https://api.github.com/user/repos || {
+            echo "[ERROR] Failed to create repository on GitHub"
+            exit 1
+        }
+        git push --mirror github || { echo "[ERROR] Pushing to GitHub failed"; exit 1; }
+    )
+
+    # Navigate back to the previous directory
+    echo "[INFO] Navigating back to the previous directory"
+    cd ..
+    echo "[END]"
+done
